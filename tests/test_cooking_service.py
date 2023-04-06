f63c03b7b82843f53ff93e8105ec054a12b62266
Eric Dasse <christ.gououi@gmail.com>
2023-04-06 18:30:17 +0200

    `main_test_order`: define pasta details directly

--- tests/test_cooking_service.py
+++ tests/test_cooking_service.py
@@ -3,7 +3,8 @@ from fredo_restaurant.dish import PastaType, Salsa, Topping
 from fredo_restaurant.order import Order
 
 
-def make_test_order(order_content):
+def make_test_order(pasta, salsa, topping=None):
+    order_content = {"pasta": pasta, "salsa": salsa, "topping": topping}
     order = Order(
         order_content=order_content,
         location="The place over there!",
@@ -17,11 +18,8 @@ def make_test_order(order_content):
 def test_cooking_service_can_take_penne_carbonara_order():
     cooking_service = CookingService()
     penne_carbonara_order = make_test_order(
-        {
-            "pasta": PastaType.PENNE,
-            "salsa": Salsa.CARBONARA,
-            "topping": None,
-        }
+        pasta=PastaType.PENNE,
+        salsa=Salsa.CARBONARA,
     )
 
     dish_to_cook = cooking_service.take_order(penne_carbonara_order)
@@ -34,11 +32,8 @@ def test_cooking_service_can_take_penne_carbonara_order():
 def test_cooking_service_can_take_spaghetti_bolognaise_order():
     cooking_service = CookingService()
     spaghetti_bolognaise_order = make_test_order(
-        {
-            "pasta": PastaType.SPAGHETTI,
-            "salsa": Salsa.BOLOGNAISE,
-            "topping": None,
-        }
+        pasta=PastaType.SPAGHETTI,
+        salsa=Salsa.BOLOGNAISE,
     )
 
     dish_to_cook = cooking_service.take_order(spaghetti_bolognaise_order)
@@ -51,11 +46,9 @@ def test_cooking_service_can_take_spaghetti_bolognaise_order():
 def test_cooking_service_can_take_penne_carbonara_with_mushrooms_order():
     cooking_service = CookingService()
     penne_carbonara_with_mushrooms = make_test_order(
-        {
-            "pasta": PastaType.PENNE,
-            "salsa": Salsa.CARBONARA,
-            "topping": Topping.MUSHROOMS,
-        }
+        pasta=PastaType.PENNE,
+        salsa=Salsa.CARBONARA,
+        topping=Topping.MUSHROOMS,
     )
 
     dish_to_cook = cooking_service.take_order(penne_carbonara_with_mushrooms)
